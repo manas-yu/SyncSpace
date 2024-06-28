6d2c555bfe8b49124f90842c2f4ed9c4f88623c8
Manas <img_2022037@iiitm.ac.in>
2024-06-28 18:50:00 +0530

    gir ignore

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 3001;
 const mongoose = require('mongoose');
 const authRouter = require('./routes/auth');
 const documentRouter = require('./routes/document');
@@ -9,12 +9,11 @@ const Document = require('./models/document');
 const http = require('http');
 var server = http.createServer(app);
 var io = require('socket.io')(server);
-const DB = "mongodb+srv://manas:1195@cluster0.rqzcak8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 app.use(cors());
 app.use(express.json())
 app.use(authRouter)
 app.use(documentRouter)
-mongoose.connect(DB).then(() => {
+mongoose.connect(process.env.DB).then(() => {
     console.log("Connection to DB successful");
 }).catch((err) => console.log(err));
 io.on('connection', (socket) => {
@@ -36,7 +35,7 @@ const autoSave = async (data) => {
     document.content = data.delta;
     document = await document.save();
 };
-server.listen(port, "0.0.0.0", () => {
-    console.log(`Server is running on port ${port}`);
-    console.log(`http://localhost:${port}`);
+server.listen(process.env.port, "0.0.0.0", () => {
+    console.log(`Server is running on port ${process.env.port}`);
+    console.log(`http://localhost:${process.env.port}`);
 });
